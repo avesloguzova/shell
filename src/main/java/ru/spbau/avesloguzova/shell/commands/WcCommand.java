240aa386e80c96a60e64653bdf627507920917bc
Alexandra Vesloguzova <sashickk@gmail.com>
2015-11-17 00:34:04 +0300

    Implements wc command

--- src/main/java/ru/spbau/avesloguzova/shell/commands/WcCommand.java
+++ src/main/java/ru/spbau/avesloguzova/shell/commands/WcCommand.java
@@ -4,9 +4,11 @@ import com.sun.istack.internal.NotNull;
 import com.sun.istack.internal.Nullable;
 import ru.spbau.avesloguzova.shell.Command;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.IntSummaryStatistics;
+import java.util.List;
 
 /**
  */
@@ -19,7 +21,21 @@ public class WcCommand implements Command {
 
     @Override
     public void execute(@NotNull String[] args, @NotNull InputStream inputStream, @NotNull OutputStream outputStream) throws IOException {
-
+        int wc = 0;
+        int lc = 0;
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(outputStream));
+        for (String fileName : args) {
+            List<String> fileLines = Files
+                    .readAllLines(Paths.get(fileName));
+            IntSummaryStatistics stats = fileLines.stream()
+                    .mapToInt(line -> line.trim().split("\\s").length).summaryStatistics();
+            wc += stats.getSum();
+            lc += stats.getCount();
+        }
+        out.write(String.format("%d %d %d",
+                args.length, lc, wc));
+        out.newLine();
+        out.flush();
     }
 
     @Override
